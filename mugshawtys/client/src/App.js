c53ade68aad0b1df597a6e7c284e0ccce8e28676
Danny <dahneel@gmail.com>
2023-05-27 21:15:42 -0600

    added basic file structuring and code as well as got auth working

---
+++ mugshawtys/client/src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import { 
+  ApolloProvider, 
+  ApolloClient, 
+  InMemoryCache, 
+  createHttpLink
+ } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <>
+          <Navbar />
+          <Routes>
+            <Route 
+              path='/' 
+              element={<Home />} 
+            />
+            {/* <Route 
+              path='/whatever' 
+              element={<Whatever />} 
+            /> */}
+            <Route 
+              path='*'
+              element={<h1 className='display-2'>Wrong page!</h1>}
+            />
+          </Routes>
+        </>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ mugshawtys/client/src/pages/Home.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Container,
+  Col,
+  Form,
+  Button,
+  Card,
+  Row
+} from 'react-bootstrap';
+// import { useMutation } from '@apollo/client';
+// import Auth from '../utils/auth';
+
+const Home = () => {
+  // create state for holding returned google api data
+ 
+  
+
+  return (
+    <>
+      <div className="text-light bg-dark p-5">
+        <Container>
+          <h1>Felonious Feelings</h1>
+            <Row>
+              <Col xs={12} md={8}>
+                <Card>
+                    <p>LOL HI</p>
+                </Card>
+              </Col>
+            </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Home;
