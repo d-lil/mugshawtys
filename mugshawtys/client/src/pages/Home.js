57ddaf2930ce0ba78ac25793e8124f0fc9118bd6
Danny <dahneel@gmail.com>
2023-05-31 13:10:26 -0600

    added booking codes to a file, we may need to break these up by state for a nested dropdown

--- mugshawtys/client/src/pages/Home.js
+++ mugshawtys/client/src/pages/Home.js
@@ -8,10 +8,38 @@ import {
   Row,
 } from 'react-bootstrap';
 import CardComp from '../components/Card';
+// import User from '../utils/User';
+
+const searchInmates = (query) => {
+  return fetch(`http://www.JailBase.com/api/1/recent/?source_id=${query}`);
+};
 
 const Home = () => {
   // create state for holding returned google api data
- 
+  // const [inmates, setInmates] = useState([]);
+  // const [searchTerm, setSearchTerm] = useState('');
+
+
+  // try {
+  //   const response = await searchInmates("jason");
+
+  //   if (!response.ok) {
+  //     throw new Error('something went wrong!');
+  //   }
+
+  //   const { items } = await response.json();
+  //   console.log(items);
+  //   const inmates = items.map((book) => ({
+  //     bookId: book.id,
+  //     authors: book.volumeInfo.authors || ['No author to display'],
+  //     title: book.volumeInfo.title,
+  //     description: book.volumeInfo.description || 'No description to display',
+  //     image: book.volumeInfo.imageLinks?.thumbnail || '',
+  //   }));
+  // };
+  // } catch (err) {
+  //   console.error(err);
+  // }
   
   return (
     <>
