1df89cd956d6aac0b52b1e0d0bb87b2a9c679354
Danny <dahneel@gmail.com>
2023-06-11 18:11:20 -0600

    moved files from inner folder out one level. got db query/transform to inmates working correctly. added modal to settings. removed preferences from settings and added image upload spot.

---
+++ client/src/pages/Inmate.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+import { QUERY_SINGLE_INMATE } from '../utils/queries';
+
+const SingleInmate = () => {
+  // Use `useParams()` to retrieve value of the route parameter `:profileId`
+  // const { inmateId } = useParams();
+  const inmateId = "4";
+  const { loading, data } = useQuery(QUERY_SINGLE_INMATE, {
+    variables: { inmateId: inmateId },
+  });
+
+  const inmate = data?.inmate || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="my-3">
+      <h3 className="card-header bg-dark text-light p-2 m-0">
+      <img src = {inmate.inmateImage} className="rounded-circle mb-3" alt="inmate image" width = "150" height = "150" />
+        {inmate.inmateName}
+      </h3>
+      <p>{inmate.inmateAge} - {inmate.inmateGender}</p>
+      <blockquote
+        className="p-4"
+        style={{
+          fontSize: '1.5rem',
+          fontStyle: 'italic',
+          border: '2px dotted #1a1a1a',
+          lineHeight: '1.5',
+        }}
+      >
+        {inmate.inmateAbout}
+      </blockquote>
+      
+    </div>
+  );
+};
+
+export default SingleInmate;
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,76 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                email
+                password
+        }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!, $age: String!, $gender: String!) {
+        addUser(username: $username, email: $email, password: $password, age: $age, gender: $gender ) {
+            token
+            user {
+                username
+                email
+                password
+                age
+                gender
+            }
+        }
+        }
+
+`;
+
+// took preferences out for now
+export const UPDATE_USER = gql`
+    mutation updateUser($image: String, $about: String) {
+        updateUser(image: $image, about: $about) {
+            image
+            about
+        }
+    }
+`;
+
+export const SAVE_INMATE = gql`
+    mutation saveInmate($inmateInfo: InmateInput) {
+        saveInmate(inmateInfo: $inmateInfo) {
+            _id
+            username
+            email
+            savedInmates {
+                inmateId
+                inmateName
+                inmateImage
+                inmateAge
+                inmateGender
+                inmateAbout
+            }
+        }
+    }
+`;
+
+export const REMOVE_INMATE = gql`
+    mutation removeInmate($inmateId: ID!) {
+        removeInmate(inmateId: $inmateId) {
+            _id
+            username
+            email
+            savedInmates {
+                inmateId
+                inmateName
+                inmateImage
+                inmateAge
+                inmateGender
+                inmateAbout
+            }
+        }
+    }
+`;
+
